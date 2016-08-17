6eb0592243a247b1252a15be37345831f5a937f5
Fabian Grimme <fbngrm@gmail.com>
2016-08-17 13:02:30 +0200

    Add post

---
+++ src/components/content/post.vue
@@ -0,0 +1,20 @@
+<template>
+  <disqus v-bind:post="post"></disqus>
+</template>
+
+<script>
+import Disqus from '../comments/disqus.vue'
+
+export default {
+  components: {
+    Disqus
+  },
+  props: ['posts']
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
