0c4827ba455f7f5d6dae89847ae9dc7b058d140a
Fabian Grimme <fbngrm@gmail.com>
2016-08-17 14:41:19 +0200

    Wrap rendered post data with single quotes

--- src/components/comments/disqus.vue
+++ src/components/comments/disqus.vue
@@ -12,8 +12,8 @@
                  */
                 
                 var disqus_config = function () {
-                    this.page.url = {{ post.meta.url }};  // Replace PAGE_URL with your page's canonical URL variable
-                    this.page.identifier = {{ post.meta.id}}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                    this.page.url = '{{ post.meta.url }}';  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = '{{ post.meta.id}}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
                 };
                 
                 (function() {  // REQUIRED CONFIGURATION VARIABLE: EDIT THE SHORTNAME BELOW
