8f10dc221052dbcd8099fabea42dad440eff3959
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 23:48:18 +0200

    Rename to absrtact to abstracts

---
+++ src/components/content/abstracts.vue
@@ -0,0 +1,30 @@
+<template>
+    <template v-for="abstract_data in abstracts_data">
+        <a v-link="{ name: 'post', params: { postname: abstract_data.meta.slug }}">
+            <blob v-bind:data="abstract_data.abstract.image"></blob>
+            <text v-bind:data="abstract_data.abstract.text"></text>
+        </a>
+    </template>
+</template>
+
+<script>
+import Text from '../media/text'
+import Blob from '../media/image'
+import abstractsData from '../../../data/abstracts.json'
+
+export default {
+  components: {
+    Text,
+    Blob
+  },
+  data () {
+    return {
+      abstracts_data: abstractsData
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
