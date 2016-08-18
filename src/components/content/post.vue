44c509b5c1d0144bbdc352e06437348ee8137907
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 14:41:27 +0200

    Add text & image rendering & diqus comments

--- src/components/content/post.vue
+++ src/components/content/post.vue
@@ -1,15 +1,31 @@
 <template>
-  <disqus v-bind:post="post"></disqus>
+  <template v-for="media in post_data.content">
+    <text v-if="media.type == 'text'" v-bind:data="media.data"></text>
+    <blob v-if="media.type == 'image'" v-bind:data="media.data"></blob>
+  </template>
+   <!-- Begin comments -->
+    <div class="columns">
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+        <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
+            <disqus shortname="post_data.meta.disqus.shortname"></disqus>
+        </div>
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+    </div> 
+    <!-- End comments -->
 </template>
 
 <script>
-import Disqus from '../comments/disqus.vue'
+import Disqus from 'vue-disqus'
+import Text from '../media/text'
+import Blob from '../media/image'
 
 export default {
   components: {
-    Disqus
+    Disqus,
+    Text,
+    Blob
   },
-  props: ['posts']
+  props: ['post_data']
 }
 </script>
 
