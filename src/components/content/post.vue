1359e819c20def4b648d33cfc3b6697b2bd7c9f8
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 23:39:48 +0200

    Add layouts to template & route to component script

--- src/components/content/post.vue
+++ src/components/content/post.vue
@@ -1,10 +1,26 @@
 <template>
-  <template v-for="media in post_data.content">
-    <text v-if="media.type == 'text'" v-bind:data="media.data"></text>
-    <blob v-if="media.type == 'image'" v-bind:data="media.data"></blob>
-  </template>
-   <!-- Begin comments -->
+    <template v-for="media in post_data.content">
+    <!-- Text columns -->
     <div class="columns">
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+        <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
+            <div class="text-left">
+                <text v-if="media.type == 'text'" v-bind:data="media.data"></text>
+            </div>
+        </div>
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+    </div>
+    <!-- End text columns -->
+
+    <!-- Image Columns -->
+    <div class="columns">
+        <blob v-if="media.type == 'image'" v-bind:data="media.data"></blob>
+    </div>
+    <!-- End Image Columns -->
+    </template>
+
+    <!-- Begin comments -->
+    <div v-if="post_data.meta.disqus" class="columns">
         <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
         <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
             <disqus shortname="post_data.meta.disqus.shortname"></disqus>
@@ -18,6 +34,7 @@
 import Disqus from 'vue-disqus'
 import Text from '../media/text'
 import Blob from '../media/image'
+import postData from '../../../data/posts.json'
 
 export default {
   components: {
@@ -25,12 +42,17 @@ export default {
     Text,
     Blob
   },
-  props: ['post_data']
+  route: {
+    data: function (transition) {
+      var postname = transition.to.params.postname
+      return {
+        post_data: postData[postname]
+      }
+    }
+  }
 }
 </script>
 
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
