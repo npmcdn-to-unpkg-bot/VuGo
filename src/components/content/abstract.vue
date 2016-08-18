eaec70893c6c553ea73d92fef065f8364f17f76f
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 16:38:28 +0200

    Add image and text rendering

---
+++ src/components/content/abstract.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-for="abstract_data in abstracts_data">
+    <blob v-bind:data="abstract_data.abstract.image"></blob>
+    <text v-bind:data="abstract_data.abstract.text"></text>
+  </div>
+</template>
+
+<script>
+import Text from '../media/text'
+import Blob from '../media/image'
+
+export default {
+  components: {
+    Text,
+    Blob
+  },
+  props: ['abstracts_data']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
