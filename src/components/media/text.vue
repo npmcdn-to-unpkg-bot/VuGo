8cff8cc8d5ce559a9f09f4471a7c232b5a1fe016
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 16:40:18 +0200

    Render data attribute in template only

--- src/components/media/text.vue
+++ src/components/media/text.vue
@@ -4,7 +4,7 @@
         <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
         <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
         <div class="text-left">
-                {{{ data.text }}}
+                {{{ data }}}
             </div>
         <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
     </div>
@@ -16,8 +16,5 @@ var marked = require('marked')
 
 export default {
   props: ['data'],
-  filters: {
-    marked: marked
-  }
 }
 </script>
