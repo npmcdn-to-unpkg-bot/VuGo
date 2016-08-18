f3f6a95eb918bb1bfd865766fced5e260a954a58
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 16:13:09 +0200

    Remove headline, subheadline & date

--- src/components/media/text.vue
+++ src/components/media/text.vue
@@ -3,14 +3,7 @@
     <div class="columns">
         <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
         <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
-            <!-- center-aligned text -->
-            <div v-if="data.headline" class="text-center">
-                <h1>{{{ data.headline }}}</h1>
-                <h2 v-if="data.subheadline" class="subheadline">{{{ data.subheadline }}}</h2>
-                <p v-if="data.date" class="ptop date">{{{ data.date }}}</p>
-            </div>
-            <!-- center-aligned text -->
-            <div class="text-left">
+        <div class="text-left">
                 {{{ data.text }}}
             </div>
         <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
@@ -19,7 +12,12 @@
 </template>
 
 <script>
+var marked = require('marked')
+
 export default {
-  props: ['data']
+  props: ['data'],
+  filters: {
+    marked: marked
+  }
 }
 </script>
