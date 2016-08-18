2122fd886a51f1ce96259f4e66c7041e012cd0cb
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 23:44:03 +0200

    Remove styles & add v-links

--- src/components/nav/nav-item.vue
+++ src/components/nav/nav-item.vue
@@ -1,5 +1,6 @@
 <template>
-  <a href="{{ item.url }}" class="nav-item" v-for="item in items">{{ item.title | uppercase }}</a>
+    <!-- use v-link directive for navigation. -->
+    <a v-link="{ path: item.slug }" class="nav-item" v-for="item in items">{{ item.title | uppercase }}</a>
 </template>
 
 <script>
@@ -10,14 +11,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-a {
-    font: 300 1.8rem 'Roboto', sans-serif;
-    color: #424242;
-    color: rgb(66, 66, 66);
-    background-color: transparent;
-    text-decoration: none;
-    border-bottom: none; 
-    padding: .7rem 1.5rem;
-}
-
 </style>
