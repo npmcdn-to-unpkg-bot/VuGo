92ca53302fa8e6b68fd15d8a439fafa35ebc887d
Fabian Grimme <fbngrm@gmail.com>
2016-08-18 16:44:22 +0200

    Move responsive div wrapper to post.vue template

--- src/components/media/text.vue
+++ src/components/media/text.vue
@@ -1,14 +1,5 @@
 <template>
-    <!-- Text columns -->
-    <div class="columns">
-        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
-        <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
-        <div class="text-left">
-                {{{ data }}}
-            </div>
-        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
-    </div>
-    <!-- End text columns -->
+{{ data }}}
 </template>
 
 <script>
