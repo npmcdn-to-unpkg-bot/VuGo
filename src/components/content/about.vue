ae9bff36bc81d3031888ff564b509ec4c2f5bf58
Fabian Grimme <fbngrm@gmail.com>
2016-08-30 13:12:13 +0200

    Change data getter, route data and template to use store state as data source

--- src/components/content/about.vue
+++ src/components/content/about.vue
@@ -1,9 +1,27 @@
 <template>
-    <template v-for="abstract_data in abs_data">
-        <a v-link="{ name: 'post', params: { postname: abstract_data.meta.slug }}">
-            <blob v-bind:data="abstract_data.abstract.image"></blob>
-            <text v-bind:data="abstract_data.abstract.text"></text>
-        </a>
+    <template v-for="media in ab_data.content">
+    <!-- Text columns -->
+    <div class="columns">
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+        <div class="column col-xs-10 col-sm-8 col-md-8 col-lg-6 col-xl-4">
+            <div class="text-left">
+                <div v-if="$loadingRouteData">Loading ...</div>
+                <template v-else>
+                <text v-if="media.type == 'text'" v-bind:data="media.data"></text>
+                </template>
+            </div>
+        </div>
+        <div class="column col-xs-1 col-sm-2 col-md-2 col-lg-3 col-xl-4 hide-xs"></div>
+    </div>
+    <!-- End text columns -->
+
+    <!-- Image Columns -->
+    <div class="columns">
+        <template v-if="!$loadingRouteData">
+        <blob v-if="media.type == 'image'" v-bind:data="media.data"></blob>
+        </template>
+    </div>
+    <!-- End Image Columns -->
     </template>
 </template>
 
@@ -28,25 +46,13 @@ export default {
   },
   data () {
     return {
-      abs_data: this.abstracts_data
+      ab_data: this.about_data
     }
   },
   route: {
     data: function (transition) {
-      var tagname = transition.to.params.tagname
-      if (!tagname) {
         return {
-          abs_data: this.abstracts_data
-        }
-      }
-      var abstractsData = this.tags_data[tagname]
-      if (!abstractsData) {
-        this.$router.go({
-          name: 'archive'
-        })
-      } else {
-        return {
-          abs_data: abstractsData
+          ab_data: this.about_data 
         }
       }
     }
